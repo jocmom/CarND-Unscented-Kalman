c9da5e3358ae75280b56fab628bd95ce2ffac462
penny <jombi@web.de>
2017-04-24 22:28:29 +0200

    ADD: first initialization of UKF variables

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -23,7 +23,13 @@ UKF::UKF() {
 
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
-
+  // values taken from course, should be changed
+  P_ <<    0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+  
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 30;
 
@@ -52,6 +58,27 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  ///* State dimension
+  n_x_ = 5;
+
+  ///* Augmented state dimension
+  n_aug_ = 7;
+  
+  ///* Number of sigma points
+  n_sigma_ = 2 * n_aug_ + 1;
+
+  ///* Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  ///* Weights of sigma points
+  weights_ = VectorXd(n_sigma_);
+
+  ///* the current NIS for radar
+  NIS_radar_ = 0;
+
+  ///* the current NIS for laser
+  NIS_laser_ = 0;
 }
 
 UKF::~UKF() {}
@@ -60,13 +87,51 @@ UKF::~UKF() {}
  * @param {MeasurementPackage} meas_package The latest measurement data of
  * either radar or laser.
  */
-void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
+void UKF::ProcessMeasurement(MeasurementPackage measurement_pack) {
   /**
   TODO:
 
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    /**
+      * Initialize the state  with the first measurement.
+      * Remember: you'll need to convert radar from polar to cartesian coordinates.
+    */
+    // first measurement
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      cout << "Kalman Filter Initialization with RADAR values " << endl;
+      //set the state with the initial location and velocity
+      float rho = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rho_dot = measurement_pack.raw_measurements_[2];
+      // Although radar gives velocity data in the form of the range rate
+      // rho_dot, a radar measurement does not contain enough information
+      // to determine the state variable velocities vx​ and v​y​​ 
+      x_ << rho*cos(phi), rho*sin(phi), 0, 0, 0; 
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      cout << "Kalman Filter Initialization with LIDAR values " << endl;
+      //set the state with the initial location and zero velocity
+      x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0, 0;
+    }
+    cout << "x input vector: " << endl << x_ << endl;
+
+    //previous_timestamp_ = measurement_pack.timestamp_;
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
 }
 
 /**
--- src/ukf.h
+++ src/ukf.h
@@ -65,6 +65,9 @@ public:
   ///* Augmented state dimension
   int n_aug_;
 
+  ///* Number of sigma points
+  int n_sigma_;
+
   ///* Sigma point spreading parameter
   double lambda_;
 
@@ -88,7 +91,7 @@ public:
    * ProcessMeasurement
    * @param meas_package The latest measurement data of either radar or laser
    */
-  void ProcessMeasurement(MeasurementPackage meas_package);
+  void ProcessMeasurement(MeasurementPackage measurement_pack);
 
   /**
    * Prediction Predicts sigma points, the state, and the state covariance
