fd6b41613e7f6606f621e29da165fc74912fb6dd
pennyheadaway <jombi@web.de>
2017-05-03 14:17:08 +0200

    ADD: Test case for PredictRadarMeasurment()

--- src/tests-ukf.cpp
+++ src/tests-ukf.cpp
@@ -91,4 +91,33 @@ TEST_CASE( "Generate sigma points", "[GenerateSigmaPoints]" ) {
   REQUIRE(ukf.P_.rows() == P_exp.rows());
   REQUIRE((ukf.P_ - P_exp).norm() < 1e-4);
 
+   //radar measurement noise standard deviation radius in m
+  ukf.std_radr_ = 0.3;
+  //radar measurement noise standard deviation angle in rad
+  ukf.std_radphi_ = 0.0175;
+  //radar measurement noise standard deviation radius change in m/s
+  ukf.std_radrd_ = 0.1; 
+  
+  VectorXd z_pred_exp(ukf.n_z_);
+  z_pred_exp <<  6.12155, 0.245993, 2.10313;
+  ukf.Xsig_pred_ << 5.9374, 6.0640, 5.925, 5.9436, 5.9266, 5.9374, 5.9389, 5.9374, 5.8106, 5.9457, 5.9310, 5.9465, 5.9374, 5.9359, 5.93744,
+      1.48, 1.4436, 1.660, 1.4934, 1.5036, 1.48, 1.4868, 1.48, 1.5271, 1.3104, 1.4787, 1.4674, 1.48, 1.4851, 1.486,
+      2.204, 2.2841, 2.2455, 2.2958, 2.204, 2.204, 2.2395, 2.204, 2.1256, 2.1642, 2.1139, 2.204, 2.204, 2.1702, 2.2049,
+      0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337, 0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188, 0.5367, 0.535048,
+      0.352, 0.29997, 0.46212, 0.37633, 0.4841, 0.41872, 0.352, 0.38744, 0.40562, 0.24347, 0.32926, 0.2214, 0.28687, 0.352, 0.318159;
+  ukf.PredictRadarMeasurement();
+  cout << "Predicted z:" << endl << ukf.z_pred_ << endl;
+  REQUIRE(ukf.z_pred_.cols() == z_pred_exp.cols());
+  REQUIRE(ukf.z_pred_.rows() == z_pred_exp.rows());
+  REQUIRE((ukf.z_pred_ - z_pred_exp).norm() < 1e-4);
+
+  MatrixXd S_exp = MatrixXd(ukf.n_z_, ukf.n_z_);
+  S_exp <<
+  0.0946171, -0.000139448, 0.00407016,
+  -0.000139448, 0.000617548, -0.000770652,
+  0.00407016, -0.000770652, 0.0180917;
+  cout << "Covariance S:" << endl << ukf.S_ << endl;
+  REQUIRE(ukf.S_.cols() == S_exp.cols());
+  REQUIRE(ukf.S_.rows() == S_exp.rows());
+  REQUIRE((ukf.S_ - S_exp).norm() < 1e-4);
 }
